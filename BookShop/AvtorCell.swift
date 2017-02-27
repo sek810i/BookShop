96b379b179518f655611d58de4ae59997c142dcc
sek810i <sek810i96@gmail.com>
2017-02-27 20:56:54 +0300

    3

---
+++ BookShop/AvtorCell.swift
@@ -0,0 +1,24 @@
+//
+//  AvtorCell.swift
+//  BookShop
+//
+//  Created by Богдан Олег on 27.02.17.
+//  Copyright © 2017 Богдан Олег. All rights reserved.
+//
+
+import UIKit
+
+class AvtorCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ BookShop/NewsViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  NewsViewCell.swift
+//  BookShop
+//
+//  Created by Богдан Олег on 27.02.17.
+//  Copyright © 2017 Богдан Олег. All rights reserved.
+//
+
+import UIKit
+
+class NewsViewCell: UICollectionViewCell {
+    
+}
